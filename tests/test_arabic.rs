75378e518731dac087573a0b57814aef746b73c3
Mate Kovacs <mkovaxx@gmail.com>
2024-04-13 13:37:04 +0900

    start testing the substitution of Arabic presentation forms

---
+++ tests/test_arabic.rs
@@ -0,0 +1,10 @@
+//! Test substitution of Arabic presentation forms
+
+use unifont::i18n::preprocess_text;
+
+#[test]
+fn test_isolated_lam_alif() {
+    let input = "\u{0644}\u{0627}";
+    let expected = "\u{FEFB} ";
+    assert_eq!(preprocess_text(input), expected);
+}
